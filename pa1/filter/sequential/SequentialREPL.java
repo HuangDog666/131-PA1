2cfa1f9f2098f2c0aee7bc591b4fe213f1448c39
Jingyun Huang <jingyunhuang@JingyundeMacBook-Pro.local>
2018-09-24 11:56:50 -0400

    Added 1 line

--- pa1/filter/sequential/SequentialREPL.java
+++ pa1/filter/sequential/SequentialREPL.java
@@ -1,11 +1,12 @@
 package cs131.pa1.filter.sequential;
 
+//Where our work goes into
 public class SequentialREPL {
 
 	static String currentWorkingDirectory;
-	
+
 	public static void main(String[] args){
-		
+
 	}
 
 }
